a18c3981c261ceec260668e96d3293a7fe361338
Carlos Neto <carlosn7to@gmail.com>
2022-10-25 17:43:04 -0300

    terminar em casa

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Exports\ReportExport;
 use App\Exports\UsersExport;
 use App\Http\Controllers\AgeRv\_aux\sales\Stars;
+use App\Models\AgeReport\Report;
 use App\Models\AgeRv\Channel;
 use App\Models\AgeRv\Collaborator;
 use App\Models\AgeRv\CollaboratorMeta;
@@ -28,6 +29,24 @@ class TestController extends Controller
 
     public function index(Request $request)
     {
+
+//        $report = Report::find(14);
+//
+//        $query = str_replace('\\', '', $report->query);
+//
+//        $array = [];
+//
+
+
+        //$result = DB::connection($report->banco_solicitado)->select($query);
+
+        $query2 = 'SELECT COLUMN_NAME
+                    FROM INFORMATION_SCHEMA.COLUMNS
+                    WHERE TABLE_NAME = \'Eventos\'';
+
+        $result = DB::connection('mysql_take')->select($query2);
+
+        return $result;
 
 
 //        $query = $request->input('query');
