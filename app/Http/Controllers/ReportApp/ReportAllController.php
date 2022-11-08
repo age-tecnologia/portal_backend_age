15eab8199163a198a69ae77d17096e4289a7fa53
Carlos Neto <carlosn7to@gmail.com>
2022-11-08 09:47:27 -0300

    novo relatorio

--- app/Http/Controllers/ReportApp/ReportAllController.php
+++ app/Http/Controllers/ReportApp/ReportAllController.php
@@ -1119,9 +1119,9 @@ class ReportAllController extends Controller
     {
         $query = 'select distinct c.id as "Nº do contrato", p."name" as "Nome do cliente",
                     (select ce2.created from contract_events ce2 where ce2.contract_id = c.id order by ce2.created desc limit 1) as "Data do bloqueio"
-                    from contracts c
-                    left join contract_events ce on ce.contract_id = c.id
-                    left join people p on c.client_id = p.id
+                    from erp.contracts c
+                    left join erp.contract_events ce on ce.contract_id = c.id
+                    left join erp.people p on c.client_id = p.id
                     where v_status = \'Bloqueio Financeiro\' and ce.contract_event_type_id = 40';
 
         $result = DB::connection('pgsql')->select($query);
