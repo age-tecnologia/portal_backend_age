25bb12ae636629b0364b9c196eba8d759ed2cae6
Carlos Neto <carlosn7to@gmail.com>
2022-11-09 15:20:43 -0300

    alteração para relatório dinâmico

--- app/Http/Controllers/AgeReport/ReportController.php
+++ app/Http/Controllers/AgeReport/ReportController.php
@@ -30,6 +30,10 @@ class ReportController extends Controller
 
     public function store(Request $request)
     {
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+        header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+
         $report = new Report();
 
         $report = $report->create([
