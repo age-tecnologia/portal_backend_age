f1d8e5c99b3760d9e8d087f9c38e1cb4a22d24e7
Carlos Neto <carlosn7to@gmail.com>
2023-01-02 10:33:59 -0300

    teste funcao atualizar base sozinha

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
 
-        $schedule->command('update:tables')->dailyAt('10:22');
+        $schedule->command('update:tables')->everyMinute();
 
     }
 
