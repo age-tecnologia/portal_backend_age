4354ba1ab5304c0cd252a1015635036e8134ead4
Carlos Alberto M. Da F. Neto <carlos.neto@tote.local>
2023-01-31 12:33:55 -0300

    melhorias age control

--- app/Http/Controllers/AgeControl/ReportsController.php
+++ app/Http/Controllers/AgeControl/ReportsController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\AgeControl;
 
+use App\Exports\ReportExport;
 use App\Http\Controllers\Controller;
 use App\Models\AgeControl\Report;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
@@ -33,6 +35,10 @@ class ReportsController extends Controller
         if($request->has('filters')) {
 
            $this->filterRepors($reports, $request->filters);
+        }
+
+        if($request->has('idReports')) {
+            return $this->downloadReports($reports, $request->input('idReports'));
         }
 
 
@@ -146,6 +152,43 @@ class ReportsController extends Controller
         }
 
         return $reports;
+    }
+
+    protected function downloadReports($reports, $idReports)
+    {
+        set_time_limit(2000);
+        ini_set('memory_limit', '2048M');
+
+
+        $result = $reports->whereIn('r.id', $idReports)->get([
+            'r.id',
+            'r.data_referencia',
+            'rp.periodo',
+            'r.quilometragem_aprovada',
+            'c.primeiro_nome',
+            'c.segundo_nome',
+            'cg.grupo',
+            'vt.tipo',
+            'v.fabricante',
+            'v.modelo'
+        ])->toArray();
+
+
+        $headers = [
+            'relato_id',
+            'relato_data_referente',
+            'relato_periodo_referente',
+            'relato_quilometragem',
+            'condutor_primeiro_nome',
+            'condutor_segundo_nome',
+            'condutor_grupo',
+            'veiculo_tipo',
+            'veiculo_fabricante',
+            'veiculo_modelo',
+        ];
+
+        return \Maatwebsite\Excel\Facades\Excel::download(new ReportExport($result, $headers), 'relatorio_relatos.xlsx');
+
     }
 }
 
