ca89b0c2c752495142179779676279cee574b1e0
Carlos Alberto M. Da F. Neto <carlos.neto@tote.local>
2023-02-10 18:07:14 -0300

    alteracao dw

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
 
-        $schedule->command('update:tables')->dailyAt('03:00');
+        $schedule->command('update:tables')->dailyAt('18:10');
 
     }
 
