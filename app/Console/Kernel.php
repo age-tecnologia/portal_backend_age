d4cdf2ad798303d372f33ab0bd78ea0ca8150cb4
Carlos Alberto M. Da F. Neto <carlos.neto@tote.local>
2023-03-30 14:06:05 -0300

    alteracao logo

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
 
-        $schedule->command('update:tables')->dailyAt('9:10');
+        $schedule->command('update:tables')->dailyAt('14:10');
 
     }
 
